7e18857613db67060d851083d81211455fb6bfbe
sabljakovich <hamza.sabljakovic@edu.fit.ba>
2020-07-28 16:53:26 +0200

    Add render endpoint, add Dockerfile, add basic logs

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+FROM node:10-alpine
+
+RUN \
+  echo "http://dl-cdn.alpinelinux.org/alpine/edge/community" >> /etc/apk/repositories \
+  && echo "http://dl-cdn.alpinelinux.org/alpine/edge/main" >> /etc/apk/repositories \
+  && echo "http://dl-cdn.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositories \
+  && apk --no-cache  update \
+  && apk --no-cache  upgrade \
+  && apk add --no-cache --virtual .build-deps \
+    gifsicle pngquant optipng libjpeg-turbo-utils \
+    udev ttf-opensans chromium \
+  && rm -rf /var/cache/apk/* /tmp/*
+
+ENV CHROME_BIN /usr/bin/chromium-browser
+ENV LIGHTHOUSE_CHROMIUM_PATH /usr/bin/chromium-browser
+ENV PUPPETEER_SKIP_CHROMIUM_DOWNLOAD="true"
+WORKDIR /usr/src/app/
+COPY . .
+
+ENV NODE_OPTIONS=--max_old_space_size=4096
+
+RUN npm i --production=false
+
+EXPOSE 1234
+ENTRYPOINT ["node"]
+CMD ["main.js"]
---
+++ main.js
@@ -0,0 +1,82 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const puppeteer = require('puppeteer')
+
+const PORT = +process.env.SERVICE_PORT || 1234;
+const app = express()
+const logger = console
+
+let browser;
+
+app.use(bodyParser.text({ type: 'text/html', limit: '100mb' }))
+
+//https://github.com/puppeteer/puppeteer/issues/1793#issuecomment-438971272
+const run = async () => {
+
+    browser = await puppeteer.launch({
+        headless: true,
+        executablePath: '/usr/bin/chromium-browser',
+        args: [
+            "--no-sandbox",
+            "--disable-gpu",
+            '--disable-dev-shm-usage',
+            '--headless'
+        ]
+    });
+
+    app.listen(PORT, () => {
+        logger.debug(`${new Date()} - PDF renderer running on port ${PORT}`)
+    });
+};
+
+run().then()
+
+
+app.get('/renderer/memory', async (req, res) => {
+
+    const used = process.memoryUsage();
+    const memory = {}
+
+    for (let key in used) {
+        memory[key] = Math.round(used[key] / 1024 / 1024 * 100) / 100;
+    }
+
+    res.status(200)
+    res.send({ memory })
+})
+
+app.post('/renderer/html-to-pdf', async (req, res) => {
+
+    logger.info(`${new Date() } - Generating a new pdf`)
+
+    const beginTimer = new Date();
+
+    try {
+        const page = await browser.newPage();
+
+        await page.setContent(req.body);
+        await page.emulateMedia('screen');
+        page.setDefaultNavigationTimeout(0)
+
+        const pdf = await page.pdf({ format: 'A4', printBackground: true });
+        await page.close();
+
+        res.set('Content-Type', 'application/pdf');
+        res.send(pdf)
+
+        logger.info(`${new Date()} - Successfully generated new pdf in ${new Date() - beginTimer} milliseconds`);
+
+    } catch (e) {
+
+        logger.error(`${new Date()} - Failed to pdf in ${new Date() - beginTimer }`, e);
+
+        res.status(500)
+        res.send({
+            errorType: 'FailedToGeneratePDF',
+            errorMessage: e.message
+        })
+    }
+});
+
+
+
